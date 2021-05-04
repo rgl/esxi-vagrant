529d0a81a8b0d866d5294c812b729a13d94ddd8b
ma <ma@sep.de>
2021-05-04 21:08:58 +0200

    changes for esxi-7.0.2

--- sysprep.sh
+++ sysprep.sh
@@ -35,52 +35,8 @@ sed -i -E '/\/(mac|virtualMac) = /d' /etc/vmware/esx.conf
 # show our changes.
 diff -u /tmp/esx.conf.orig /etc/vmware/esx.conf || true
 
-# configure the system to automatically set the vmk0 mac address to
-# the vmnic0 mac address.
-cat >/etc/rc.local.d/local.sh <<'LOCAL_SH'
-#!/bin/sh
+# get rid of harcodet mac address settins
+esxcli system settings advanced set -o /Net/FollowHardwareMac -i 1
 
-# local configuration options
-
-# Note: modify at your own risk!  If you do/use anything in this
-# script that is not part of a stable API (relying on files to be in
-# specific places, specific tools, specific output, etc) there is a
-# possibility you will end up with a broken system after patching or
-# upgrading.  Changes are not supported unless under direction of
-# VMware support.
-
-# Note: This script will not be run when UEFI secure boot is enabled.
-
-python3 <<'EOF'
-import csv
-import io
-import subprocess
-import sys
-
-def get_csv_property(command, filter_property_name, filter_property_value, filter_property_output_name):
-    p = subprocess.Popen(
-        command,
-        stdout=subprocess.PIPE,
-        stderr=subprocess.STDOUT)
-    for row in csv.DictReader(io.TextIOWrapper(p.stdout)):
-        if row[filter_property_name] == filter_property_value:
-            return row[filter_property_output_name]
-
-vmk0_mac = get_csv_property(
-    ('esxcli', '--formatter=csv', 'network', 'ip', 'interface', 'list'),
-    'Name',
-    'vmk0',
-    'MACAddress')
-vmnic0_mac = get_csv_property(
-    ('esxcli', '--formatter=csv', 'network', 'nic', 'list'),
-    'Name',
-    'vmnic0',
-    'MACAddress')
-
-if vmk0_mac != vmnic0_mac:
-    print('setting vmk0 mac address to %s...' % vmnic0_mac)
-    subprocess.run(('esxcli', 'network', 'ip', 'interface', 'set', '-i', 'vmk0', '-e', 'false'))
-    subprocess.run(('sed', '-i', '-E', r's,^(/net/vmkernelnic/child\[0000\]/mac) = .+,\1 = "%s",g' % vmnic0_mac, '/etc/vmware/esx.conf'))
-    subprocess.run(('esxcli', 'network', 'ip', 'interface', 'set', '-i', 'vmk0', '-e', 'true'))
-EOF
-LOCAL_SH
+# make changes permanent
+/sbin/auto-backup.sh
