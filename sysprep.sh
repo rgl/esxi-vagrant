0df3b059fcbe87cf578d81737670a0defb6b0be0
Rui Lopes <rgl@ruilopes.com>
2021-09-28 08:50:00 +0100

    add note about ESXi 6.7+ exposing vnc over websockets

--- sysprep.sh
+++ sysprep.sh
@@ -6,6 +6,8 @@ set -euxo pipefail
 # vmware-iso packer builder:
 #   * enable guest ARP inspection to get their IP addresses (aka the Guest IP Hack).
 #   * configure the firewall to allow vnc connections (5900-6000 ports).
+#     NB ESXi 6.7+ exposes VNC through a websocket (at the same 443 port as the
+#        api), so this firewall part is no longer needed and should be removed.
 # see https://github.com/umich-vci/packer-vib
 esxcli software acceptance set --level=CommunitySupported
 esxcli software vib install -v https://github.com/umich-vci/packer-vib/releases/download/v1.0.0-1/packer.vib
